f59fa5ce661f233ec8341bdcfe8a04f2a2b23b14
Paing Zaw Bhone <paingzaw991@gmail.com>
2026-08-11 16:16:09 +0700

    added loading indicator and created api for real data and connected to the app.

--- lib/post_provider.dart
+++ lib/post_provider.dart
@@ -1,92 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-// ChangeNotifier gives this class the ability to alert the UI when data changes
 class PostProvider extends ChangeNotifier {
-  //added 'id' to easily find exactly which post gets upvoted
-  final List<Map<String, dynamic>> _posts = [
-    {
-      'id': '1',
-      'uploaderName': 'Dr. S John',
-      'role': 'Lecturer',
-      'timeAgo': '1 hour ago',
-      'category': '📌 Announcements',
-      'content':
-          'Attention Students: The Object-Oriented Programming midterm grades have been posted. Please check the portal. We will review the Java practical questions in tomorrow\'s lecture.',
-      'upvotes': 45,
-      'comments': 12,
-    },
-    {
-      'id': '2',
-      'uploaderName': 'Larry',
-      'role': 'Student',
-      'timeAgo': '3 hours ago',
-      'category': '📚 Academic',
-      'content':
-          'Does anyone have a good visual guide for how the Buddy System memory allocation works? Struggling with this Computer Architecture concept.',
-      'upvotes': 8,
-      'comments': 3,
-    },
-    {
-      'id': '3',
-      'uploaderName': 'Hein Htet',
-      'role': 'Student',
-      'timeAgo': '3 hours ago',
-      'category': '📚 Academic',
-      'content':
-          'Does anyone have a good visual guide for how the Buddy System memory allocation works? Struggling with this Computer Architecture concept.',
-      'upvotes': 8,
-      'comments': 3,
-    },
-    {
-      'id': '4',
-      'uploaderName': 'Wathan Oo',
-      'role': 'Student',
-      'timeAgo': '3 hours ago',
-      'category': '🎮 Social',
-      'content':
-          'Need a break from coding! Anyone want to queue up for some Counter-Strike 2 competitive matches tonight around 8 PM?',
-      'upvotes': 15,
-      'comments': 7,
-    },
-    {
-      'id': '5',
-      'uploaderName': 'Htoo Aung Lin',
-      'role': 'Student',
-      'timeAgo': '3 hours ago',
-      'category': '📚 Academic',
-      'content':
-          'Does anyone have a good visual guide for how the Buddy System memory allocation works? Struggling with this Computer Architecture concept.',
-      'upvotes': 8,
-      'comments': 3,
-    },
-    {
-      'id': '6',
-      'uploaderName': 'Alex',
-      'role': 'Student',
-      'timeAgo': '5 hours ago',
-      'category': '🎮 Social',
-      'content':
-          'Need a break from coding! Anyone want to queue up for some Counter-Strike 2 competitive matches tonight around 8 PM?',
-      'upvotes': 15,
-      'comments': 7,
-    },
-  ];
+  List<Map<String, dynamic>> _posts = [];
+  bool _isLoading = false;
 
   List<Map<String, dynamic>> get posts => _posts;
+  bool get isLoading => _isLoading;
 
-  // 1. Logic to upvote a post
-  void upvotePost(String postId) {
-    final index = _posts.indexWhere((post) => post['id'] == postId);
-    if (index != -1) {
-      _posts[index]['upvotes'] += 1; // Increase the count
+  //fetches data when the app starts
+  PostProvider() {
+    fetchPosts();
+  }
+
+  Future<void> fetchPosts() async {
+    // 1. Set loading to true and update the UI
+    _isLoading = true;
+    notifyListeners();
+
+    try {
+      // 2. HTTP request
+      final url = Uri.parse(
+        'https://api.jsonbin.io/v3/b/6a7ae643da38895dfed4db0b',
+      );
+      final response = await http.get(url);
+
+      if (response.statusCode == 200) {
+        // 3. Decode the JSON response
+        final data = json.decode(response.body);
+
+        final List<dynamic> loadedPosts = data['record']['posts'];
+
+        // 4. Convert the dynamic list back into our required Map format
+        _posts = loadedPosts
+            .map((post) => post as Map<String, dynamic>)
+            .toList();
+      } else {
+        print('Failed to load posts. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      print('Error fetching data: $error');
+    } finally {
+      // 5. Turn off the loading state and rebuild the feed
+      _isLoading = false;
       notifyListeners();
     }
   }
 
-  // 2. Logic to publish a brand new post
+  // Logic to upvote a post
+  void upvotePost(String postId) {
+    final index = _posts.indexWhere((post) => post['id'] == postId);
+    if (index != -1) {
+      _posts[index]['upvotes'] += 1;
+      notifyListeners();
+    }
+  }
+
+  // publish a brand new post
   void addPost(String category, String content) {
     _posts.insert(0, {
-      'id': DateTime.now().toString(), // Generates a unique ID
+      'id': DateTime.now().toString(),
       'uploaderName': 'CS BANANA',
       'role': 'Student',
       'timeAgo': 'Just now',
@@ -95,6 +69,6 @@ class PostProvider extends ChangeNotifier {
       'upvotes': 0,
       'comments': 0,
     });
-    notifyListeners(); // Tells the Home tab feed to update and show the new post
+    notifyListeners();
   }
 }
